677aab9cf8b7ed9f45315641082ae3508c937069
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-09-23 05:08:20 -0400

    Switched import to require for libs to work.

--- src/build-tools/build.ts
+++ src/build-tools/build.ts
@@ -1,33 +1,34 @@
-import minimist from 'minimist';
-import sortJson from 'sort-json';
-import glob from 'glob';
+const minimist = require('minimist');
+const sortJson = require('sort-json');
+const glob = require('glob');
 
 const args = minimist(process.argv.slice(2));
 const { env } = args;
 
 const runTasks = (callback: (env: string) => void): void => {
-    callback(env);
+  callback(env);
 };
 const globOptions: any = {
-    ignore: '**/node_modules/**',
-    realPath: true
+  ignore: '**/node_modules/**',
+  realPath: true
 }
 const sortJsonFiles = (): void => {
-    glob('**/*.json', globOptions, (er, files) => {
-        if (er) {
-            console.error(er);
-        }
-        files.forEach((fileName, index) => {
-            try {
-                sortJson.overwrite(fileName);
-            } catch (err) {
-                console.error(err);
-            }
-        });
-
+  glob('**/*.json', globOptions, (er, files) => {
+    if (er) {
+      console.error(er);
+    }
+    files.forEach((fileName, index) => {
+      console.log(fileName);
+      try {
+        sortJson.overwrite(fileName);
+      } catch (err) {
+        console.error(err);
+      }
     });
+
+  });
 };
 
 export {
-    runTasks, sortJsonFiles
+  runTasks, sortJsonFiles
 }
