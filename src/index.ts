37f13a3dcd4c4b9becfddb25066a290ac36a4de6
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-02-28 02:10:49 -0500

    Factored out merchant service integration validation into the SDK.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './contact';
 export * from './customer';
 export * from './enums';
 export * from './http';
+export * from './merchant';
 export * from './session';
 export * from './transaction';
 export * from './util';
---
+++ src/merchant/validate.ts
@@ -0,0 +1,24 @@
+import { SwitchContext } from 'ns8-switchboard-interfaces';
+import { ServiceIntegrationType } from 'ns8-protect-models';
+
+/**
+ * Asserts that the merchant has the expected integration
+ * @param context - the Switch Context
+ * @param integrationType - a known service integration type
+ * @throws if no match is found
+ * @returns true if a match is found
+ */
+export const validateMerchantIntegration = (
+  context: SwitchContext,
+  integrationType: ServiceIntegrationType,
+): boolean => {
+  const match = context.merchant?.serviceIntegrations?.find((integration) => {
+    return integration.type === integrationType;
+  })?.type;
+
+  if (match !== integrationType)
+    throw new Error(
+      `No ${integrationType} Service Integration Type defined on this merchant "${context.merchant?.name}"`,
+    );
+  return true;
+};
