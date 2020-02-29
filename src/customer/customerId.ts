0304b743a8bb33e8c4c0ac5a908812c07895b2b4
Christopher Froehlich <christopher.froehlich@ns8.com>
2020-02-28 20:28:17 -0500

    Make parameters required and updated doc block. https://github.com/ns8inc/protect-sdk-switchboard/pull/6#discussion_r385968025

--- src/customer/customerId.ts
+++ src/customer/customerId.ts
@@ -2,11 +2,11 @@ import { v4 as uuidv4, v5 as uuidv5 } from 'uuid';
 
 /**
  * Validates that a provided customer id is present, else generates a unique UUID for the customer
- * @param customerId - The ID value provided by the platform
- * @param emailAddress - an email address, if any is available
+ * @param customerId - The ID value provided by the platform. Pass `''` if none is available.
+ * @param emailAddress - an email address, if any is available. Pass `''` if none is available.
  * @returns either (a) the original customer Id, if set or (b) a unique UUID based on the email address or (c) a random, unique UUID
  */
-export const getUniqueCustomerId = (customerId = '', emailAddress = ''): string => {
+export const getUniqueCustomerId = (customerId: string, emailAddress: string): string => {
   let id = customerId;
   // If we don't yet have a customer's id, generate one
   if (!id) {
