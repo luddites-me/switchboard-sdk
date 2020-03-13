d1287354cc7c811aa0618460d755e586656778d2
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-03-13 12:00:41 -0400

    Dropping deprecated API support for retry loops.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './contact';
 export * from './customer';
 export * from './enums';
-export * from './http';
 export * from './merchant';
 export * from './order';
 export * from './session';
