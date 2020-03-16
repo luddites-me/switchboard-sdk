73e399750d3e0cf628b595f64a7ecfd90f6f455e
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-03-16 11:16:38 -0400

    Fixed toSession() logic.

--- src/session/toSession.ts
+++ src/session/toSession.ts
@@ -1,4 +1,6 @@
 import { Session } from 'ns8-protect-models';
+import { UAParser } from 'ua-parser-js';
+const isIp = require('is-ip');
 
 export interface SessionData {
   acceptLanguage?: string;
@@ -10,6 +12,14 @@ export interface SessionData {
 
 export const toSession = (data: SessionData): Session => {
   const { acceptLanguage, ip, screenHeight, screenWidth, userAgent } = data;
+  if (!isIp(ip)) {
+    throw new Error(`${ip} is not a valid IP Address, and IP Address is required.`);
+  }
+  const ua = new UAParser(userAgent);
+  if (!ua.getUA()) {
+    throw new Error(`${userAgent} is not a valid User Agent string, and a UA string is required.`);
+  }
+
   const session = new Session({ ip, userAgent });
   if (acceptLanguage) {
     session.acceptLanguage = acceptLanguage;
