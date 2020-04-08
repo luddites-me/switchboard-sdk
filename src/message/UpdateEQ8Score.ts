6d03d3c04bb55ebfd6fb9969383a3e14a275b72b
Chris Doriott <chris.doriott@ns8.com>
2020-04-08 16:48:32 -0700

    Updating logic for data queue pushes CH-22726

--- src/message/UpdateEQ8Score.ts
+++
@@ -1,6 +0,0 @@
-import { MessageBase } from './MessageBase';
-
-/**
- * Message to update an order's EQ8 score
- */
-export type UpdateEQ8Score = MessageBase;
--- src/message/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './MessageBase';
-export * from './UpdateEQ8Score';
-export * from './UpdateOrderRisk';
-export * from './UpdateOrderStatus';
