a786e61a0660fa71f989d5df58a5af68881e12b5
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-13 13:21:49 -0400

    Restore filename.

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,7 +7,8 @@ import {
 const config = getWebpackConfig({
   bundleTarget: BundleTarget.NODE,
   distDirectory: './dist',
-    // Globals hack to address https://github.com/node-formidable/formidable/issues/337#issuecomment-183388869
+  fileName: 'index.js',
+  // Globals hack to address https://github.com/node-formidable/formidable/issues/337#issuecomment-183388869
   globals: [{ name: 'GENTLY', value: false }],
   libraryName: 'index',
   mode: BundleMode.DEVELOPMENT,
