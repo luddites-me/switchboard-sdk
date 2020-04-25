a29bcdb79cfa71a4411fff898248e901f06d05a2
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-25 17:12:02 -0400

    Fixed another lint error.

--- src/customer/toCustomer.test.ts
+++ src/customer/toCustomer.test.ts
@@ -1,4 +1,7 @@
-/* eslint-disable no-unused-expressions */
+/* eslint-disable
+  no-unused-expressions,
+  sonarjs/no-duplicate-string,
+ */
 import { expect, use } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import 'mocha';
