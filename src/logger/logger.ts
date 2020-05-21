7c69ba90d63e8a93c3639b9bff3fa6a71bdb471b
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-05-21 12:06:23 -0400

    Lint fix. Updated code owners.

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -4,7 +4,7 @@ import { Logger, LoggerOptions, createLogger, format, transports } from 'winston
 /**
  * NOTE: these properties need to be explicitly exported for downstream consumers.
  * Since they come from a 3rd party library, we can safely ignore them from code coverage.
-*/
+ */
 export {
   Logger,
   LoggerOptions,
