203b7f94fb8340caefc409ab08deccf27554cb1d
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-06-16 11:32:24 -0400

    Tidy up the async forEach method.

--- src/util/async.ts
+++ src/util/async.ts
@@ -10,9 +10,12 @@ export const sleep = async (milliseconds = 1000): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
-export const asyncForEach = async (array, callback) => {
-  if (!array || !callback) { return; }
-  for (let index = 0; index < array.length; index++) {
+/**
+ * Mechanism to perform async/await operations over an array
+ */
+export const asyncForEach = async (array: any[], callback: any): Promise<void> => {
+  if (!array || !callback) return;
+  for (let index: number = 0; index < array.length; index += 1) {
     await callback(array[index], index, array);
   }
 }
